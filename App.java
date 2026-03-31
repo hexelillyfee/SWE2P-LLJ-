4bdc0104a9930e9e58d2e1c5bb4ec60af95ea6d2
Lilly <lit4219@thi.de>
2026-03-31 15:17:05 +0200

    Weiterer Student erstellt

--- App.java
+++ App.java
@@ -4,17 +4,18 @@ public class App {
     public static void main(String[] args) {
         Studierender moritz = new Studierender("Moritz", "00175252", "WINF-B");
         Studierender antonia = new Studierender("Antonia", "00175253", "INFO-B");
+        Studierender sophia = new Studierender("Sophia", "006767", "WINF-B");
         Dozierender kroeninger = new Dozierender("Jutta Kröninger", "I", "B203", 16);
 
 
-        moritz.frageStellen(kroeninger, "Was ist ihr Lieblingstier?");
-        kroeninger.frageBeantworten(moritz, "Hasen");
+        sophia.frageStellen(kroeninger, "Was ist ihr Lieblingstier?");
+        kroeninger.frageBeantworten(sophia, "Hasen");
 
         moritz.sprechstundeAnfrage(kroeninger, 16);
 
         moritz.feedbackAbgeben(kroeninger, 9);
         antonia.feedbackAbgeben(kroeninger, 5);
-        moritz.feedbackAbgeben(kroeninger, 7);
+        sophia.feedbackAbgeben(kroeninger, 7);
 
         kroeninger.printFeedbackDurchschnitt();
 
