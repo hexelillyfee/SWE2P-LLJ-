a28b45a64530e5c96366b90bb3fb7a933c227cb3
Joshua <joshi230804@gmail.com>
2026-04-07 11:28:42 +0200

    Praktikumsstunde vollständig

---
+++ Praktikumsstunde.java
@@ -0,0 +1,30 @@
+public class Praktikumsstunde {
+     private String thema;
+    private String datum;
+    private String uhrzeit;
+    private String raum;
+    private String[] aufgaben;
+
+    Praktikumsstunde(String thema, String datum, String uhrzeit, String raum, String[] aufgaben){
+        this.thema = thema;
+        this.datum = datum;
+        this.uhrzeit = uhrzeit;
+        this.raum = raum;
+        this.aufgaben = aufgaben;
+    }
+
+
+    public void showDetails(){
+        System.out.println("\t\tThema: " + this.thema);
+        System.out.println("\t\tDatum: " + this.datum);
+        System.out.println("\t\tUhrzeit: " + this.uhrzeit + "Uhr");
+        System.out.println("\t\tRaum: " + this.raum);
+        System.out.println("\t\tAufgaben: ");
+        for(int i = 0; i < this.aufgaben.length; i++){
+            if(this.aufgaben[i] != null){
+                System.out.println("\t\t\tAufgabe " + (i+1) + ": " + aufgaben[i]);
+            }
+        }
+    }
+}
+
