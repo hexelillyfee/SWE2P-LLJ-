34a5d10eb02e15478985ee200adc07b956924642
Joshua <joshi230804@gmail.com>
2026-04-07 12:11:42 +0200

    verbesserung benamung und print-outs

--- App.java
+++ App.java
@@ -16,16 +16,16 @@ public class App {
      System.out.println("\n---\n- Sprechstunden -------------------------------------\n---");
         max.sprechstundeAnfrage(bauer, 10);
 
-    //Abgabe von Feedback
+    
         System.out.println("\n---\n- Feedback -------------------------------------------\n---");
-        max.feedbackAbgeben(bauer, 9);
-        paul.feedbackAbgeben(bauer, 5);
-        anna.feedbackAbgeben(bauer, 7);
+        max.feedbackAbgeben(bauer, 10);
+        paul.feedbackAbgeben(bauer, 6);
+        anna.feedbackAbgeben(bauer, 8);
 
-        //Durschnitt des Feedbacks abfragen
+       
         bauer.printFeedbackDurchschnitt();
 
-        //erstellen einer lehrveranstaltung
+        
         System.out.println("\n---\n- Lehrveranstaltungen --------------------------------\n---");
         Lehrveranstaltung gestaltung = new Lehrveranstaltung("Gestaltung", bauer, 2); 
         
@@ -37,12 +37,12 @@ public class App {
         anna.lehrveranstaltungAbmeldung(gestaltung);
         paul.lehrveranstaltungAnmeldung(gestaltung);
 
-        gestaltung.addVorlesungsstunde("Gestaltgesetze", "20.04.2026", "9:15", "G116");
-        gestaltung.addVorlesungsstunde("Icons", "27.04.2026", "9:15", "G116");
+        gestaltung.addVorlesungsstunde("Gestaltgesetze", "12.04.2026", "8:15", "G116");
+        gestaltung.addVorlesungsstunde("Icons", "20.04.2026", "9:15", "G116");
         
         
-        String[] praktikumsaufgaben ={"UI Beispiele für Gestaltgesetze finden" , "Moodboard erstellen"};
-        gestaltung.addPraktikumsstunde("Gestaltgesetze", "21.04.2026", "9:00", "G116", praktikumsaufgaben);
+        String[] praktikumsaufgaben ={"Gute unde schlechte UI Beispiele im mobile Design finden" , "Beispiele festhalten"};
+        gestaltung.addPraktikumsstunde("UI Design", "21.04.2026", "9:00", "G116", praktikumsaufgaben);
         
         
         gestaltung.showDetails();
